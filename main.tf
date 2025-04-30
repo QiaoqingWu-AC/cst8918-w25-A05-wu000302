f6e731e0314430da5d80eabf168605a86a7bbe59
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-04-29 20:39:19 -0400

    update

--- main.tf
+++ main.tf
@@ -131,7 +131,7 @@ resource "azurerm_linux_virtual_machine" "web_vm" {
 
   admin_ssh_key {
     username = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("/mnt/d/wuqq/Documents/lv2/8918/ssh/id_rsa.pub")
   }
 
   os_disk {
